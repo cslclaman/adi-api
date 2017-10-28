5238685976a6a733ee60793f9db3572cb13947d4
Caique Siqueira <cslclaman@gmail.com>
2017-10-28 16:52:28 -0200

    Remove overloaded constructor and gets/sets

--- model/image.py
+++ model/image.py
@@ -1,65 +1,71 @@
 class Image:
-	def __init__(self, id=0, md5="", filePath=""):
-		self.__id = id
-		self.__md5 = md5
-		self.__filePath = filePath
 
-	#def __init__(self, dbline):
-	#	self.__id = dbline[0]
-	#	self.__md5 = dbline[1]
+	def __init__(self, row):
+		self.__id = row[0]
+		self.__md5 = row[1]
+		self.__filePath = row[2]
+		self.__tagString = row[3]
+		self.__rating = row[4]
+		self.__active = True if row[5] == 1 else False
+		self.__fileSize = row[6]
+		self.__fileSource = row[7]
+		self.__creationDate = row[8]
+		self.__lastUpdate = row[9]
+		#self.__primarySource = row[10]
+		self.__sourceName = row[11]
 
-	def getId():
-		return __id
+	def getId(self):
+		return self.__id
 	def setId(self,id):
 		self.__id = id
 
-	def getMd5():
-		return __md5
+	def getMd5(self):
+		return self.__md5
 	def setMd5(self,md5):
 		self.__md5 = md5
 
-	def getFilePath():
-		return __filePath
+	def getFilePath(self):
+		return self.__filePath
 	def setFilePath(self,filePath):
 		self.__filePath = filePath
 
-	def getTagString():
-		return __tagString
+	def getTagString(self):
+		return self.__tagString
 	def setTagString(self,tagString):
 		self.__tagString = tagString
 
-	def getRating():
-		return __rating
+	def getRating(self):
+		return self.__rating
 	def setRating(self,rating):
 		self.__rating = rating
 
-	def isActive():
-		return __active
+	def isActive(self):
+		return self.__active
 	def setActive(self,active):
 		self.__active = active
 
-	def getFileSize():
-		return __fileSize
+	def getFileSize(self):
+		return self.__fileSize
 	def setFileSize(self,fileSize):
 		self.__fileSize = fileSize
 
-	def getFileSource():
-		return __fileSource
+	def getFileSource(self):
+		return self.__fileSource
 	def setFileSource(self,fileSource):
 		self.__fileSource = fileSource
 
-	def getCreationDate():
-		return __creationDate
+	def getCreationDate(self):
+		return self.__creationDate
 	def setCreationDate(self,creationDate):
 		self.__creationDate = creationDate
 
-	def getLastUpdate():
-		return __lastUpdate
+	def getLastUpdate(self):
+		return self.__lastUpdate
 	def setLastUpdate(self,lastUpdate):
 		self.__lastUpdate = lastUpdate
 
-	def getSourceName():
-		return __sourceName
+	def getSourceName(self):
+		return self.__sourceName
 	def setSourceName(self,sourceName):
 		self.__sourceName = sourceName
 
