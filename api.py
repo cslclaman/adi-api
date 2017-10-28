2bf67af23ac3c400941fc7db1304f357a51ce8a0
Caique Siqueira <cslclaman@gmail.com>
2017-10-28 21:51:28 -0200

    Add List All Images endpoint for API

--- api.py
+++ api.py
@@ -1,29 +1,26 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify, request
-import MySQLdb
 from model.image import Image
+from control.dbcontrol import ImageControl
 
 app = Flask(__name__)
-images = []
-con = MySQLdb.connect(host="localhost",user="appuser",passwd="ms987654",database="adi6_db")
-
-cursor = con.cursor()
 
 @app.route('/')
 def index():
 	return """<h1>API</h1>
+	<ul>
+	<li><b>/image/list</b> = List all images</li>
 	"""
 
 @app.route('/image/list', methods=['GET'])
 def image_list():
-	cursor.execute("Select * from image limit 100")
-	results = cursor.fetchall()
-	print("{0} {1:10} {2} {3}".format('ID', 'MD5', 'PATH'))
-
-	for row in results:
-		print(row)
-		#produtos.append(model.produto(row))
-	return jsonify(results), 200
+	ctr = ImageControl()
+	jsonImageList = []
+	page = request.args.get('page', 1, type=int)
+	tags = request.args.get('tags', "", type=str)
+	for image in ctr.get_image_list(page,tags):
+		jsonImageList.append(image.serialize())
+	return jsonify(jsonImageList), 200
 
 if __name__ == "__main__":
 	app.run(debug=True, host="localhost")
