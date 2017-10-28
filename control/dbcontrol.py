e335a1c4194811b36d57fb4d854ad1e29004b1ee
Caique Siqueira <cslclaman@gmail.com>
2017-10-28 21:48:51 -0200

    Modify list images method (now it accepts page and tags!)

--- control/dbcontrol.py
+++ control/dbcontrol.py
@@ -8,15 +8,19 @@ USER = 'appuser'
 PASS = 'ms987654'
 DB = 'adi6_db'
 
+LIMIT = 25
+
 class ImageControl:
     def __init__(self):
         self.__con = MySQL.connect(user=USER, password=PASS, host=HOST, database=DB)
         self.__cursor = self.__con.cursor()
 
-    def get_image_list(self):
+    def get_image_list(self, pagenum, tags):
         listImages = []
         try:
-            self.__cursor.execute("SELECT * FROM Image")
+            page = (pagenum - 1) * LIMIT
+            query = "SELECT * FROM Image WHERE tag_string like \'{0}\' LIMIT {1},{2}".format(("%"+tags+"%"),page,LIMIT)
+            self.__cursor.execute(query)
             results = self.__cursor.fetchall()
             for row in results:
                 image = Image(row)
