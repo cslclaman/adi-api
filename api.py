6c3c27a618d3bf7aeb68e7659a9e8048fb30aadb
Caique Siqueira <cslclaman@gmail.com>
2017-10-29 16:01:53 -0200

    Add endpoints to get a single image by ID or (beta) by MD5

--- api.py
+++ api.py
@@ -14,10 +14,30 @@ def image_list():
 	ctr = ImageControl()
 	jsonImageList = []
 	page = request.args.get('page', 1, type=int)
+	limit = request.args.get('limit', 25, type=int)
 	tags = request.args.get('tags', "", type=str)
-	for image in ctr.get_image_list(page,tags):
+	for image in ctr.get_image_list(tags,page,limit):
 		jsonImageList.append(image.serialize())
 	return jsonify(jsonImageList), 200
+
+@app.route('/image/<int:id>', methods=['GET'])
+def image_by_id(id):
+	ctr = ImageControl()
+	image = ctr.get_image_by_id(id)
+	if image is None:
+		return jsonify({})
+	else:
+		return jsonify(image.serialize()), 200
+
+@app.route('/image/get', methods=['GET'])
+def image_get():
+	ctr = ImageControl()
+	md5 = request.args.get('md5', "00000000000000000000000000000000", type=str)
+	image = ctr.get_image_by_md5(md5)
+	if image is None:
+		return jsonify({})
+	else:
+		return image_by_id(image.getId())
 
 if __name__ == "__main__":
 	app.run(debug=True, host="localhost")
