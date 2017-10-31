26a93b93e4fafb8ff5f1d6acb505a8e659d54769
Caique Siqueira <cslclaman@gmail.com>
2017-10-31 00:37:03 -0200

    Add methods to create image and to get image source.

--- control/dbcontrol.py
+++ control/dbcontrol.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-
 from model.image import Image
+from model.imagesource import ImageSource
+from datetime import datetime
 import mysql.connector as MySQL
 
 HOST = 'localhost'
@@ -15,7 +16,7 @@ class ImageControl:
         self.__con = MySQL.connect(user=USER, password=PASS, host=HOST, database=DB)
         self.__cursor = self.__con.cursor()
 
-    def get_image_list(self, tags, pagenum, limit):
+    def getList(self, tags, pagenum, limit):
         listImages = []
         try:
             if (limit > MAX_LIMIT): limit = MAX_LIMIT
@@ -29,10 +30,9 @@ class ImageControl:
                 listImages.append(image)
         except MySQL.Error as err:
             print(err)
-
         return listImages;
 
-    def get_image_by_id(self, id):
+    def getById(self, id):
         try:
             query = "SELECT * FROM Image WHERE id = {0}".format(id)
             self.__cursor.execute(query)
@@ -45,13 +45,62 @@ class ImageControl:
             print(err)
             return None
 
-    def get_image_by_md5(self, md5):
+    def getByMd5(self, md5):
         try:
             query = "SELECT * FROM Image WHERE md5 = \'{0}\'".format(md5)
             self.__cursor.execute(query)
             row = self.__cursor.fetchone()
             if row is not None:
                 return Image(row)
+            else:
+                return None
+        except MySQL.Error as err:
+            print(err)
+            return None
+
+    def create(self, image):
+        id = image.getId()
+        md5 = image.getMd5()
+        file_path = image.getFilePath()
+        tag_string = image.getTagString()
+        rating = image.getRating()
+        active = 1 if image.isActive() == True else 0
+        file_size = image.getFileSize()
+        creation_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        file_source = image.getFileSource()
+        columns = "md5,file_path,tag_string,rating,active,file_size,creation_date,file_source"
+        values = "\'{0}\',\'{1}\',\'{2}\',\'{3}\',{4},{5},\'{6}\',\'{7}\'".format(md5,file_path,tag_string,rating,active,file_size,creation_date,file_source)
+        try:
+            query = "INSERT INTO Image ({0}) VALUES ({1})".format(columns, values)
+            self.__cursor.execute(query)
+        except MySQL.Error as err:
+            print(err)
+
+class ImageSourceControl:
+    def __init__(self):
+        self.__con = MySQL.connect(user=USER, password=PASS, host=HOST, database=DB)
+        self.__cursor = self.__con.cursor()
+
+    def getList(self, image_id, name):
+        listImageSources = []
+        try:
+            query = "SELECT * FROM Image_Source WHERE image = {0} and source_name like \'{1}\'".format(image_id, ("%"+name+"%"))
+            self.__cursor.execute(query)
+            results = self.__cursor.fetchall()
+            for row in results:
+                imagesrc = ImageSource(row)
+                listImageSources.append(imagesrc)
+        except MySQL.Error as err:
+            print(err)
+        return listImageSources;
+
+    def getById(self, id):
+        try:
+            query = "SELECT * FROM Image_Source WHERE id = {0}".format(id)
+            self.__cursor.execute(query)
+            row = self.__cursor.fetchone()
+            if row is not None:
+                return ImageSource(row)
             else:
                 return None
         except MySQL.Error as err:
