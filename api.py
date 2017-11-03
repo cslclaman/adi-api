6d404c53d5bcd0b06533ab0cd76f6c7955e22ab5
Caique Siqueira <cslclaman@gmail.com>
2017-11-03 16:01:06 -0200

    Add endpoint to get unique Tag by ID or name (tag attribute). Close #8

--- api.py
+++ api.py
@@ -118,5 +118,23 @@ def tag_view(id):
 		else:
 			return jsonify(tag.serialize()), 200
 
+@app.route('/tag', methods=['GET'])
+def tag_get():
+	id = request.args.get('id', -1, type=int)
+	name = request.args.get('name', "", type=str)
+	showadi = request.args.get('adi_tag', "hide", type=str)
+	tag = None
+
+	if id != -1:
+		tag = ctrTag.getById(id)
+	else:
+		if name != "":
+			tag = ctrTag.getByTagName(name)
+
+	if tag is None:
+		return jsonify({}), 400
+	else:
+		return redirect(url_for("tag_view",id = tag.getId(), adi_tag = showadi))
+
 if __name__ == "__main__":
 	app.run(debug=True, host="localhost")
