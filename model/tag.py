e724b3457aca66a8c99f086c9a4d1c98f9f5a640
Caique Siqueira <cslclaman@gmail.com>
2017-11-03 17:29:21 -0200

    Modify init and add getters and setters for Tag class

--- model/tag.py
+++ model/tag.py
@@ -1,14 +1,39 @@
 class Tag:
-    def __init__(self, row):
-        self.__id = row[0]
-        self.__tag = row[1]
-        self.__url = row[2]
-        self.__adiTag = row[3]
+    def __init__(self, row=None, id=0, tag="", url=""):
+        if row is not None:
+            self.__id = row[0]
+            self.__tag = row[1]
+            self.__url = row[2]
+            self.__adiTag = row[3]
+        else:
+            self.__id = id
+            self.__tag = tag
+            if url:
+                self.__url = url
+            else:
+                self.__url = tag
+            self.__adiTag = None
 
     def getId(self):
         return self.__id
+    def setId(self, id):
+        self.__id = id
+
+    def getTag(self):
+        return self.__tag
+    def setTag(self, tag):
+        self.__tag = tag
+
+    def getUrl(self):
+        return self.__url
+    def setUrl(self, url):
+        self.__url = url
+
     def getAdiTagId(self):
         return self.__adiTag
+    def setAdiTagId(self, adiTag):
+        self.__adiTag = adiTag
+
     def hasAdiTag(self):
         return self.__adiTag is not None
 
