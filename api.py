7ec955eec2aadfab77d3f2c35ac65e023a3d65b0
Caique Siqueira <cslclaman@gmail.com>
2017-11-03 14:17:43 -0200

    Change "show something" values in Image and Tag (now are strings, default "hide". Close #1.

--- api.py
+++ api.py
@@ -23,25 +23,35 @@ def image_list():
 	page = request.args.get('page', 1, type=int)
 	limit = request.args.get('limit', 25, type=int)
 	tags = request.args.get('tags', "", type=str)
-	source = request.args.get('source', False, type=bool)
+	source = request.args.get('source', "hide", type=str)
 	rating = request.args.get('rating', "", type=str)
 
 	for image in ctrImage.getList(tags,page,limit,rating):
-		if source:
-			imgSource = ctrImgSource.getById(image.getPrimarySourceId())
-			jsonImageList.append(image.serialize(imgSource))
+		#the idea is to change Image serialize method to receive a list of sources to serialize
+		#(if this code was Java, it's be an overloaded method, but... Python is python and overload is not too handful)
+		#the list is to be created here
+		if source == "show":
+			#add all ImageSources related to this image in the list
+			imageSource = ctrImgSource.getById(image.getPrimarySourceId())
+			jsonImageList.append(image.serialize(imageSource))
+		elif source == "primary":
+			#add just primary source in list
+			imageSource = ctrImgSource.getById(image.getPrimarySourceId())
+			jsonImageList.append(image.serialize(imageSource))
 		else:
+			#well, the list here is empty.
 			jsonImageList.append(image.serialize())
 	return jsonify(jsonImageList), 200
 
 @app.route('/image/<int:id>', methods=['GET'])
 def image_by_id(id):
-	source = request.args.get('source', False, type=bool)
+	source = request.args.get('source', "hide", type=str)
 	image = ctrImage.getById(id)
 	if image is None:
 		return jsonify({}), 400
 	else:
-		if source:
+		#same as above
+		if source == "show" or source == "primary":
 			imgSource = ctrImgSource.getById(image.getPrimarySourceId())
 			return jsonify(image.serialize(imgSource)), 200
 		else:
@@ -64,12 +74,12 @@ def tag_list():
 	name = request.args.get('name', "", type=str)
 	searchType = request.args.get('search', "all", type=str)
 	searchTag = request.args.get('association', "", type=str)
-	showAdiTag = request.args.get('adi_tag', False, type=bool)
+	showadi = request.args.get('adi_tag', "hide", type=str)
 
 	adiTag = ctrAdiTag.getByTagDictionary(formats.parseAdiTag(searchTag))
 
 	for tag in ctrTag.getList(page,limit,name,searchType,adiTag):
-		if showAdiTag and tag.hasAdiTag():
+		if showadi == "show" and tag.hasAdiTag():
 			adiTag = ctrAdiTag.getById(tag.getAdiTagId())
 			jsonTagList.append(tag.serialize(adiTag))
 		else:
@@ -79,12 +89,12 @@ def tag_list():
 
 @app.route('/tag/<int:id>', methods=['GET'])
 def tag_by_id(id):
-	showadi = request.args.get('adi_tag', False, type=bool)
+	showadi = request.args.get('adi_tag', "hide", type=str)
 	tag = ctrTag.getById(id)
 	if tag is None:
 		return jsonify({}), 400
 	else:
-		if showadi and tag.hasAdiTag():
+		if showadi == "show" and tag.hasAdiTag():
 			adiTag = ctrAdiTag.getById(tag.getAdiTagId())
 			return jsonify(tag.serialize(adiTag)), 200
 		else:
